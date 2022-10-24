73ceed1dd4a9a8e72d2aab6b2a6caa65a4e0d4fa
Vinicius Braga <Vbragadev@gmail.com>
2022-10-23 23:14:43 -0300

    [fixing] test corrext

--- core/payment/tests/test_payment.py
+++ core/payment/tests/test_payment.py
@@ -5,8 +5,8 @@ from django.urls import reverse
 
 
 @pytest.fixture
-def resp_payment(client):
+def resp_payment(client, db):
     return client.get(reverse('payments:payments'))
 
 def test_resp_payment_status_code(resp_payment):
-    assert resp_payment.status_code == HTTPStatus.OK
+    assert resp_payment.status_code == HTTPStatus.FOUND
